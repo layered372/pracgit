a3255f214b690e75ceece0d5b347001e0bf16ef8
Kim Sangjin <layered372@gmail.com>
2023-06-01 00:05:45 +0900

    add add()

--- main2.c
+++ main2.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
+int add()
+{
+   int a = 100;
+   int b = 200;
+
+   return a + b;
+}
+
 int main(void)
 {
     printf("hello eden\n");
 
-    int i = 100;
+    int res = add();
+    printf("res: %d\n", rest);
 
     return 0;
 }
